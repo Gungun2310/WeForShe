8d283dd98d10e0eaa4d06eaa8853b9c33e026540
Fatima Jamil <fatimajamil453@gmail.com>
2024-07-13 17:26:01 +0530

    hidden file fixed

--- scroll.js
+++ scroll.js
@@ -1,5 +1,45 @@
-document.getElementById('show-list-button').addEventListener('click', function() {
-    const itemList = document.getElementById('item-list');
+document.getElementById('show-list-button1').addEventListener('click', function() {
+    const itemList = document.getElementById('item-list1');
+    if (itemList.classList.contains('hidden')) {
+        itemList.classList.remove('hidden');
+    } else {
+        itemList.classList.add('hidden');
+    }
+});
+document.getElementById('show-list-button2').addEventListener('click', function() {
+    const itemList = document.getElementById('item-list2');
+    if (itemList.classList.contains('hidden')) {
+        itemList.classList.remove('hidden');
+    } else {
+        itemList.classList.add('hidden');
+    }
+});
+document.getElementById('show-list-button3').addEventListener('click', function() {
+    const itemList = document.getElementById('item-list3');
+    if (itemList.classList.contains('hidden')) {
+        itemList.classList.remove('hidden');
+    } else {
+        itemList.classList.add('hidden');
+    }
+});
+document.getElementById('show-list-button4').addEventListener('click', function() {
+    const itemList = document.getElementById('item-list4');
+    if (itemList.classList.contains('hidden')) {
+        itemList.classList.remove('hidden');
+    } else {
+        itemList.classList.add('hidden');
+    }
+});
+document.getElementById('show-list-button5').addEventListener('click', function() {
+    const itemList = document.getElementById('item-list5');
+    if (itemList.classList.contains('hidden')) {
+        itemList.classList.remove('hidden');
+    } else {
+        itemList.classList.add('hidden');
+    }
+});
+document.getElementById('show-list-button6').addEventListener('click', function() {
+    const itemList = document.getElementById('item-list6');
     if (itemList.classList.contains('hidden')) {
         itemList.classList.remove('hidden');
     } else {
